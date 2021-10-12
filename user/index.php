0a1d62f7e4086a14ae462cc6cf4214fcff59faf0
chipolito <chipolitoh@gmail.com>
2021-10-12 18:27:38 -0500

    Actualizacon, prueba de fork

--- user/index.php
+++ user/index.php
@@ -16,7 +16,7 @@
         <div class="container col-xxl-8 px-4 py-5">
             <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
                 <div class="col-10 col-sm-8 col-lg-6">
-                    <img src="../../assets/img/user/default.jpg" class="d-block mx-lg-auto img-fluid shadow" id="userPhoto" alt="User picture" width="700" height="500" loading="lazy">
+                    <img src="../assets/img/user/default.jpg" class="d-block mx-lg-auto img-fluid shadow" id="userPhoto" alt="User picture" width="700" height="500" loading="lazy">
                 </div>
                 <div class="col-lg-6">
                     <h1 class="fw-bold lh-1 mb-0" id="userName">Cuauhtemoc Hernandez</h1>
