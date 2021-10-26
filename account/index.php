c4bf174f0a0390ca171268b2b5a0be85607767f5
chipolito <chipolitoh@gmail.com>
2021-10-25 19:03:43 -0500

    activar paginacion
    Cuando exsita mas de 20 registros se debe paginar las tablas

--- account/index.php
+++ account/index.php
@@ -88,7 +88,7 @@
                         </ul>
                     </div>
                 </nav>
-                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4" id="mainContenedor"></main>
+                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pb-4" id="mainContenedor"></main>
             </div>
         </div>
 
@@ -100,6 +100,7 @@
 
         <!-- Datatables -->
         <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
 
         <script type="text/javascript">
             var currentLeague = null;
