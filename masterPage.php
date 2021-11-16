bad64ae21acf47811b19c40b41a30e37d472912c
chipolito <chipolitoh@gmail.com>
2021-11-16 15:56:34 -0600

    Varios
    Traduccion y header para master page

--- masterPage.php
+++ masterPage.php
@@ -20,9 +20,32 @@
     </head>
 <body>
     <!-- Header -->
-    <div>
-        <hr>
+    <header class="p-3 mb-3 bg-dark border-bottom">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start ">
+        <a href="javascript:void(0);" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none text-white">
+          <span class="fs-4">Clubtres</span>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0"></ul>
+
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+          <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+        </form>
+
+        <div class="dropdown text-end">
+          <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+          </a>
+          <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1" style="">
+            <li><a class="dropdown-item" href="<?php echo $base_url; ?>/account/setting.html">Settings</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Sign out</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
+  </header>
 
     <!-- Body: Aqui se imprime todo el contedido de las paginas secundarias capturados del buffer -->
     <?php echo $content; ?>
