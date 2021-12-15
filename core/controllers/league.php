453707a608724a55727283619bff14b2aa01a39d
chipolito <60354138+chipolito@users.noreply.github.com>
2021-12-15 17:10:14 -0600

    actualizacion de juego
    Se activa la edicion de juegos, cancelacion y eliminacion

--- core/controllers/league.php
+++ core/controllers/league.php
@@ -267,6 +267,25 @@
                 header('HTTP/1.1 200 Ok');
                 header("Content-Type: application/json; charset=UTF-8");                
                 exit(json_encode($response));
+            }else if($_POST['_method'] == 'updateEvent2'){
+                $leagueModel = new Leaguemodel();
+                $tmpResponse = $leagueModel->updateEvent2($_POST);
+
+                if($tmpResponse[0]){
+                    $response = array(
+                        'codeResponse'  => 200,
+                        'message'       => 'Registered event'
+                    );
+                }else{
+                    $response = array(
+                        'codeResponse'  => 0,
+                        'message'       => 'Unregistered event'
+                    );
+                }
+
+                header('HTTP/1.1 200 Ok');
+                header("Content-Type: application/json; charset=UTF-8");                
+                exit(json_encode($response));
             }else if($_POST['_method'] == 'sendSolicitud'){
                 $equiposData    = json_decode($_POST["equiposData"]);
                 $leagueModel    = new Leaguemodel();
